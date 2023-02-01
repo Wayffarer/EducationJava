47456a310b8b40764509c08668374527d180cd2e
Wayffarer <hihvoin@mail.ru>
2023-02-01 14:45:39 +0300

    OOP

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-//        Примеры работы методов
-//        info("Hello");
-//        String java="Java";
-//        info(java);
+        Transport bmw = new Transport();
+        bmw.setValues(250.5f,2500,"Red",new byte[] {0 ,0 ,0});
+        String res1 = bmw.getValues();
+        System.out.println(res1);
+//        bmw.speed = 250.5f;
+//        bmw.weight = 2500;
+//        bmw.color = "Red";
+//        bmw.coordinate = new byte[] {0 ,0 ,0};
 
-//        short num=7;
-//        int result1=summa((short)5,num);
-//
-//        short num2=13;
-//        int result2=summa((short)4,num2);
-//        info(String.valueOf(result2));
-
-        byte[] nums1=new byte[]{5,6,8};
-
-        int summ1=summaAray(nums1);
-        System.out.println("Сумма: "+summ1);
-
-        byte[] nums2=new byte[]{5,12,5,32,6,8};
-
-        int summ2=summaAray(nums2);
-        System.out.println("Сумма: "+summ2);
+        Transport truck = new Transport();
+        truck.speed = 140.5f;
+        truck.weight = 5600;
+        truck.color = "Blue";
+        truck.coordinate = new  byte[]{100, 0, 100};
+        String res2= truck.getValues();
+        System.out.println(res2);
     }
-
-    public static int summaAray(byte[] arr){
-        int summa=0;
-        for(byte i =0; i<arr.length;i++){
-            summa +=arr[i];
-        }
-        return summa;
-    }
-
-//    public static int summa(short a, short b){
-//        int res= a+b;
-//        String result="Результат: "+res;
-//        info(result);
-//        return res;
-//    }
-//    public static void info(String word){
-//        System.out.print(word);
-//        System.out.println("!");
-//    }
 }
