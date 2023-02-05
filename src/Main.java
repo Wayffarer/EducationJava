df4117e74566dba2459429a5a18714c1d268ed13
Wayffarer <hihvoin@mail.ru>
2023-02-05 17:42:44 +0300

    Class constructors

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,13 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        //Одномерные масивы
-//        int[] nums = new int[5];
-//        nums[0] = 124;
-//        nums[1] = 34;
-//        nums[2] = 12;
-//        nums[3] = 2;
-//        nums[4] = 87;
-//        int res = nums[2] * 2;
-//        System.out.println(nums[2] + "\n" + res);
-//
-//        float[] nums2 = new float[]{5.5f, 6.75f, 12.43f, 125.32f, 91,23f};
-//
-//        for (int i = 0; i < nums2.length; i++){
-//            System.out.println(nums2[i]);
-//        }
+        Transport bmw = new Transport(250.5f,2500,"Red",new byte[] {0 ,0 ,0});
 
-//        int[] arr = new int[4];
-//        Scanner scanner = new Scanner(System.in);
-//
-//        for (int i = 0; i < arr.length; i++){
-//            System.out.print("Ведите число: ");
-//            int value = scanner.nextInt();
-//            arr[i] = value;
-//        }
-//        int min = arr[0];
-//        for (int i = 0; i < arr.length; i++){
-//            if (arr[i] < min)
-//                min = arr[i];
-//        }
-//        System.out.println("Minimal: " + min);
-        //многомерные
-        char[][] syms = new char[2][2];
-        syms[0][0] = 't';
-        System.out.println(syms[0][0]);
-
-        byte[][] nums = new byte[][]{
-                {3,5},
-                {12,31},
-                {7,11},
-        };
-            nums[1][1] = 23;
-        System.out.println(nums[1][1]);
+         Transport truck = new Transport(5600,new  byte[]{100, 0, 100});
+//        truck.speed = 140.5f;
+//        truck.weight = 5600;
+//        truck.color = "Blue";
+//        truck.coordinate = new  byte[]{100, 0, 100};
+//        String res2= truck.getValues();
+//        System.out.println("Truck: "+res2);
     }
 }
---
+++ src/Transport.java
@@ -0,0 +1,31 @@
+public class Transport {
+    private float speed;
+    private int weight;
+    private String color;
+    private byte[] coordinate;
+    public Transport(float speed, int weight, String color, byte[] coordinate){
+//        Join to DB
+        System.out.println("Object created");
+        setValues(speed, weight, color, coordinate);
+        System.out.println(getValues());
+    }
+    public Transport(int weight, byte[] coordinate){
+        this.weight =weight;
+        this.coordinate = coordinate;
+        System.out.println(getValues());
+    }
+    public void setValues(float speed, int weight, String color, byte[] coordinate){
+        this.speed = speed;
+        this.weight =weight;
+        this.color = color;
+        this.coordinate = coordinate;
+    }
+    public String getValues() {
+        String info = "Object speed: " + speed + ". Object weight: " + weight + ". Object color: " + color + "." + "\n";
+        String infoCoordinates = "Coordinates:\n";
+        for (int i = 0; i < coordinate.length; i++) {
+            infoCoordinates += coordinate[i] + "\n";
+        }
+        return info + infoCoordinates;
+    }
+}
